de8920a3b135f0bb2f78fbf353bebe805ed7f44a
MissAmbiguity <melimora1205@gmail.com>
2023-10-27 21:30:40 -0700

    UPDATE

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: Prints a string using puts
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(1, "Programming is like building a multilingual puzzle\n", 49);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
