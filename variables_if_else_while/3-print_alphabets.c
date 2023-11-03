7fdf17994e4ec27c77178387ec602d456889dc82
MissAmbiguity <melimora1205@gmail.com>
2023-11-03 14:05:19 -0700

    UPDATE

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char low;
+	
+	for (low = 'a'; low <= 'z'; low++)
+	putchart(low);
+
+	for (low = 'A'; low <'z'; low ++)
+		putchar(low);
+		putchar('\n');
+		
+		return(0)
