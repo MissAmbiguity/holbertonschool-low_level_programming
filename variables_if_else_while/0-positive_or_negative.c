8890f24da019ccdfa4c5ba924aa682640305f027
MissAmbiguity <melimora1205@gmail.com>
2023-11-03 09:27:30 -0700

    UPDATE

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
+	printf("%d is ", n);
+
+    if (n > 0) {
+        printf("positive\n");
+    } else if (n < 0) {
+        printf("negative\n");
+    } else {
+        printf("zero\n");
+    }
 	return (0);
 }
