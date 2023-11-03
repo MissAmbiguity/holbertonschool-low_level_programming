f20788c684baebb6e64a0e84416f0e1724991489
MissAmbiguity <melimora1205@gmail.com>
2023-11-03 14:28:37 -0700

    UPDATE

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,16 @@
 */
 int main(void)
 {
-char low;
+char low, high;
 
 for (low = 'a'; low <= 'z'; low++)
 {
 putchar(low);
+}
+
+for (high = 'A'; high <= 'Z'; high++)
+{
+putchar(high);
 }
 
 putchar('\n');
