8a24abe5eacfdab837b914d144d3db94d5487142
MissAmbiguity <melimora1205@gmail.com>
2023-11-03 13:45:35 -0700

    UPDATE

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,16 @@
 /**
 * main - Prints the alphabetic
 *
-* Return: Always (Success)
+* Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
-	char c;
-	for (c='a'; c <= 'z'; c++)
-	putchar(c);
-	putchar('\n');
-	return (0);
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+
+putchar('\n');
+return (0);
 }
