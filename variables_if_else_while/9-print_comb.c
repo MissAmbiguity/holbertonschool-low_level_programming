47475128a109dbf89a72c8224330ad2672a8fc87
MissAmbiguity <melimora1205@gmail.com>
2023-11-03 16:15:31 -0700

    UPDATE

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main (void)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
+int d;
 
-	int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
 
-	for (d= '0'; d <= '9'; d++)
-	{
-	putchar(d);
-	if (d !='9')
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
-	return (0);
+return (0);
 }
 
