ca719a7db738c58dc5f4311c347c7fcb64d78e96
MissAmbiguity <melimora1205@gmail.com>
2023-11-03 18:14:02 -0700

    UPDATE

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,6 +3,7 @@
 /**
 * print_alphabet - Prints the lowercase alphabet followed by a new line
 */
+
 void print_alphabet(void)
 {
 char letter = 'a';
@@ -12,7 +13,5 @@ while (letter <= 'z')
 _putchar(letter);
 letter++;
 }
-
 _putchar('\n');
 }
-
