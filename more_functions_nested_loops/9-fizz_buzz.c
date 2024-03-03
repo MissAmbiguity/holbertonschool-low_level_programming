34a1cf84dadd5dd0edea7b6e902745da965ce8e5
MissAmbiguity <melimora1205@hotmail.com>
2024-03-02 23:41:22 -0500

    UPDATE

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,35 +7,35 @@
  */
 int main(void)
 {
-    int x;
+	int x;
 
-    for (x = 1; x <= 100; x++)
-    {
-        if ((x % 3 == 0) && (x % 5 == 0))
-        {
-            printf("FizzBuzz");
-        }
-        else if (x % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (x % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", x);
-        }
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
 
-        if (x != 100)
-        {
-            printf(" ");
-        }
-    }
+		if (x != 100)
+		{
+			printf(" ");
+		}
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
 
